167092582c5666bcf2df64e8f47ecdbce6cc69f1
sstingo <48621517+sstingo@users.noreply.github.com>
2020-11-17 18:04:19 +0800

    行程 玩家 場地調整

--- code/main.py
+++ code/main.py
@@ -1,16 +1,22 @@
 import multiprocessing as mp
 import time
 from opencv import *
-# from scene import *
+from window import *
 
 if __name__ == '__main__':
-    camera = Camera()
-    process = mp.Process(target=camera.captureFace)
-    process.start()
 
-    # view = Scene()
-    # ball = Ball(view.floor, view.net)
-    # ball.throwBall()
+    scene = Scene()
+    ball = Ball(scene.floor, scene.net)
+    # camera = Camera()
+    player = Player()
 
-    process.join()
+    pb = mp.Process(target=ball.throwBall)
+    # pc = mp.Process(target=camera.captureFace)
+
+    pb.start()
+    # pc.start()
+
+    pb.join()
+    # pc.join()
+
     print("done")
--- code/window.py
+++ code/window.py
@@ -3,21 +3,21 @@ import numpy as np
 
 
 class Scene:
-    W = 9                 # 地板寬度
-    L = 18                # 地板長度
+    W = 12                # 地板寬度
+    L = 24                # 地板長度
     g = 9.8               # 重力加速度 9.8 m/s^2
 
-    scene = canvas(title="皮卡丘打排球", width=800, height=500, x=0, y=0,
-                   center=vec(0, 5, 0), background=vec(0, 0.6, 0.6))
+    view = canvas(title="皮卡丘打排球", width=800, height=500, x=0, y=0,
+                  center=vec(0, 5, 0), background=vec(0, 0.6, 0.6))
     floor = box(pos=vec(0, 0, 0), size=vec(
         W, 0.01, L), texture=textures.metal)
-    net = box(pos=vec(0, 1.5, 0), size=vec(
-        W, 3, 0.01), texture=textures.metal)
+    net = box(pos=vec(0, 2.5, 0.1), size=vec(
+        W, 5, 0.2), texture=textures.metal)
 
 
 class Ball:
-    W = 9                 # 地板寬度
-    L = 18                # 地板長度
+    W = 12                 # 地板寬度
+    L = 24                # 地板長度
     g = 9.8               # 重力加速度 9.8 m/s^2
 
     def __init__(self, floor, net):
@@ -35,19 +35,37 @@ class Ball:
         ball = sphere(pos=vec(0, 5, self.L/4), radius=size, color=color.red, make_trail=True,
                       v=vec(0, v0 * sin(theta), -v0 * cos(theta)), a=vec(0, -self.g, 0))
 
-        while(ball.pos.z > -self.L/2):
+        while(1):
             rate(1000)
             ball.v += ball.a * dt
             ball.pos += ball.v * dt
 
             if(ball.pos.y - self.floor.pos.y <= size + 0.5 * self.floor.height and ball.v.y < 0):
                 i += 1
-                print(i, t, ball.pos.z)
+                # print(i, t, ball.pos.z)
                 ball.v.y = -ball.v.y
+
+            if(ball.pos.z > self.L/2 or ball.pos.z < -self.L/2):  # 測試
+                ball.v.y = -ball.v.y
+                ball.v.z = -ball.v.z
 
             t += dt
 
 
+class Player:
+    pikachu = box(pos=vec(0, 1.5, 9), size=vec(2, 3, 2), texture=textures.wood)
+
+    # def __init__(self, floor, net):
+    #     self.floor = floor
+    #     self.net = net
+
+    # def move(self):
+
+
 if __name__ == '__main__':
     print("test import")
-#print(t, ball.pos.y)
+
+    scene = Scene()
+    player = Player()
+
+# ball.pos.z > -self.L/2
